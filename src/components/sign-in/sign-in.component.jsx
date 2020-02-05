fd0ee8fe0848352f62e35f3972026a09e4094cf5
54 Bridge <54bridgeapp@gmail.com>
2020-02-05 12:42:50 -0600

    added sign up/sign in components

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export default class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>Email</label>
+          <input
+            name="email"
+            type="email"
+            value={email}
+            onChange={this.handleChange}
+            required
+          />
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={this.handleChange}
+            required
+          />
+
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
---
+++ src/pages/sign-in-and-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { SignIn } from "../../components";
+
+export const SignInAndSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
