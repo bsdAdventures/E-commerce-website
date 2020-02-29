f5e9fe499a6bd9ad9a10685fbc82de57e3109bae
54 Bridge <31743169+buchidiai@users.noreply.github.com>
2020-02-29 12:43:23 -0600

    initial setup of sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,12 +1,24 @@
-import { takeEvery, call, put, all } from "redux-saga/effects";
+import { takeLatest, call, put } from "redux-saga/effects";
 import { ShopActionTypes } from "./shop.types";
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from "../shop";
+import { firestore, convertCollectionsSnapshotToMap } from "../../firebase";
 
 export function* fetchCollectionsAsync() {
-  yield console.log("iam fire");
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
-  yield takeEvery(
+  yield takeLatest(
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
